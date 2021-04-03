ab682fc7c3960e89cded80cb4d13c5daf3338b22
vadimdoga <vadimca37@gmail.com>
2021-04-03 22:27:25 +0300

    add new component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
 import SignIn from "./auth/SignIn"
-import Courses from "./courses/Courses"
+import Courses from "./courses/general/Courses"
+import NewCourse from "./courses/new/NewCourse"
 
 function App() {
     return (
         <div className="App">
             <Switch>
                 <Route exact path="/" component={SignIn} />
+                <Route path="/courses/new" component={NewCourse} />
                 <Route path="/courses" component={Courses} />
-
             </Switch>
         </div>
     );
